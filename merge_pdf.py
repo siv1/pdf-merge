7ab8578dd546611058d512daf0ff267655344de3
siv <siavash.nasr2@gmail.com>
2016-08-26 16:52:57 +1000

    Changed specific references to more general ones.

--- merge_pdf.py
+++ merge_pdf.py
@@ -1,18 +1,20 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader, PdfFileMerger
 
 
-def merge_title(title_pdf, body_pdf):
+def merge_pdfs(first_pdf, second_pdf):
     """
     Merge title page and user guide body.
+    At the moment, only have functionality for
+    2 PDF files, intend to expand this
     """
     merger = PdfFileMerger()
-    pdfs = [title_pdf, body_pdf]
+    pdfs = [first_pdf, second_pdf]
     for f in pdfs:
         merger.append(open(f, 'rb'))
 
     merger.write(open('combined.pdf', 'wb'))
 
-title_pdf = 'title.pdf'
-body_pdf = 'manRev1.1.pdf'
+first_pdf = 'your_pdf_name.pdf'
+second_pdf = 'your_pdf_name.pdf'
 
 merge_title(title_pdf, body_pdf)
